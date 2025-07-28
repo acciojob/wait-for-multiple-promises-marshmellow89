284d432e7d9ac6122f5f02880b959964feb0aafb
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-28 13:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,71 +1,31 @@
-// function createPromise(promiseNumber){
-// 	return new Promise(resolve => {
-// 		const time = Math.random()*2+1;
-// 		setTimeout(() => {
-// 			resolve({promiseNumber, time: time.toFixed(3)
-// 					});
-// 		}, time*1000);
-// 	});
-// }
-
-// Promise.all([createPromise(1), createPromise(2), createPromise(3)
-// 			]).then(result => {
-// 	const output = document.querySelector("#output").innerHTML="";
-// 	  const totalTime = Math.max(...results.map(res => parseFloat(res.time))).toFixed(3);
-
-//   // Populate the table with the results
-//   results.forEach(result => {
-//     const row = `<tr>
-//                    <td>Promise ${result.promiseNumber}</td>
-//                    <td>${result.time}</td>
-//                  </tr>`;
-//     output.innerHTML += row;
-//   });
-
-//   // Add the total row
-//   const totalRow = `<tr>
-//                       <td>Total</td>
-//                       <td>${totalTime}</td>
-//                     </tr>`;
-//   output.innerHTML += totalRow;
-// });
-// 			})
-
-
-function createPromise(promiseNumber) {
-  return new Promise((resolve) => {
-    const time = Math.random() * 2 + 1; // Random time between 1 and 3 seconds
-    setTimeout(() => {
-      resolve({ promiseNumber, time: time.toFixed(3) });
-    }, time * 1000);
-  });
+function createPromise(promiseNumber){
+	return new Promise(resolve => {
+		const time = Math.random()*2+1;
+		setTimeout(() => {
+			resolve({promiseNumber, time: time.toFixed(3)
+					});
+		}, time*1000);
+	});
 }
 
 Promise.all([
-  createPromise(1),
-  createPromise(2),
-  createPromise(3)
-]).then(results => {
-  // Remove the "Loading..." row
-  const output = document.getElementById('output');
-  output.innerHTML = ''; // Clear the loading row
+	createPromise(1),
+	createPromise(2),
+	createPromise(3)
+			]).then(results => {
+	const output = document.querySelector("#output").innerHTML="";
+	  const totalTime = Math.max(...results.map(res => parseFloat(res.time))).toFixed(3);
 
-  // Calculate the total time
-  const totalTime = Math.max(...results.map(res => parseFloat(res.time))).toFixed(3);
+	results.forEach(({promiseNumber, time}) => {
+		document.createElement("tr").innerHTML=`<td>Promise ${promiseNumber}</td>
+		<td>${time}</td>`;
+		output.appendChild(tr);
+	});
 
-  // Populate the table with the results
-  results.forEach(result => {
-    const row = `<tr>
-                   <td>Promise ${result.promiseNumber}</td>
-                   <td>${result.time}</td>
-                 </tr>`;
-    output.innerHTML += row;
-  });
 
-  // Add the total row
-  const totalRow = `<tr>
-                      <td>Total</td>
-                      <td>${totalTime}</td>
-                    </tr>`;
-  output.innerHTML += totalRow;
-});
+	const totalRow = document.createElement("tr");
+	totalRow.innerHTML = `<td><strong>Total</strong></td>
+	<td><strong>${totalTime}</strong></td>`;
+
+	output.appendChild(totalRow);
+
