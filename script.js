bb12f78c2e8e2d69f8f9f3ada652449eadf50924
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-28 15:30:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-function createPromise(promiseNumber){
-	return new Promise(resolve => {
-		const time = Math.random()*2+1;
-		setTimeout(() => {
-			resolve({promiseNumber, time: time.toFixed(3)
-					});
-		}, time*1000);
-	});
-}
+// function createPromise(promiseNumber){
+// 	return new Promise(resolve => {
+// 		const time = Math.random()*2+1;
+// 		setTimeout(() => {
+// 			resolve({promiseNumber, time: time.toFixed(3)
+// 					});
+// 		}, time*1000);
+// 	});
+// }
 
-Promise.all([
-	createPromise(1),
-	createPromise(2),
-	createPromise(3)
-			]).then(results => {
-	const output = document.querySelector("#output");
-	output.innerHTML="";
+// Promise.all([
+// 	createPromise(1),
+// 	createPromise(2),
+// 	createPromise(3)
+// 			]).then(results => {
+// 	const output = document.querySelector("#output");
+// 	output.innerHTML="";
 	
 
-	results.forEach(({promiseNumber, time}) => {
-	const tr = document.createElement("tr");
-		tr.innerHTML=`<td>Promise ${promiseNumber}</td>
-		<td>${time}</td>`;
-		output.appendChild(tr);
-	});
+// 	results.forEach(({promiseNumber, time}) => {
+// 	const tr = document.createElement("tr");
+// 		tr.innerHTML=`<td>Promise ${promiseNumber}</td>
+// 		<td>${time}</td>`;
+// 		output.appendChild(tr);
+// 	});
 
-  const totalTime = Math.max(...results.map(res => parseFloat(res.time))).toFixed(3);
+//   const totalTime = Math.max(...results.map(res => parseFloat(res.time))).toFixed(3);
 	
-	const totalRow = document.createElement("tr");
-	totalRow.innerHTML = `<td><strong>Total</strong></td>
-	<td><strong>${totalTime}</strong></td>`;
+// 	const totalRow = document.createElement("tr");
+// 	totalRow.innerHTML = `<td><strong>Total</strong></td>
+// 	<td><strong>${totalTime}</strong></td>`;
 
-	output.appendChild(totalRow);
+// 	output.appendChild(totalRow);
+
+
 
